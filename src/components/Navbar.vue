3eac6bcee6d5269e8e314f5d7a53ecbfbcd4e7fd
maranix <ramanverma4183@gmail.com>
2024-03-13 00:30:21 +0530

    Navbar component and begin

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import NavbarItem from './NavbarItem.vue'
+</script>
+
+<template>
+  <nav>
+    <NavbarItem href="/" text="/" />
+    <div class="nav--links">
+      <NavbarItem href="works" text="Works" />
+      <NavbarItem href="blogs" text="Blogs" />
+      <NavbarItem href="about" text="About" />
+      <NavbarItem href="contact" text="Contact" />
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.nav--links {
+    width: 50%;
+
+    display: flex;
+  justify-content: end;
+}
+
+</style>
---
+++ src/components/NavbarItem.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps({
+    href: String,
+    text: String
+})
+</script>
+
+<template>
+    <a :href="props.href">{{ props.text }}</a>
+</template>
+
+<style scoped>
+a {
+    min-width: 100px;
+    margin: 0 1.5rem;
+
+    font: 500 1.5rem var(--heading-font-family);
+    text-align: center;
+
+    transition: font-weight 0.1s;
+}
+
+a:hover {
+    font-weight: 900;
+}
+</style>
